9e754de81253831603c34762610f5a3b9d9d183d
wsderte <31801595+wsderte@users.noreply.github.com>
2023-02-25 18:58:38 +0200

    fix bug with Date

--- lib/getDate.ts
+++ lib/getDate.ts
@@ -3,10 +3,17 @@ export const GetDate: () => string = () => {
 
     date.setHours(date.getHours() + 2)
 
-    const createDate =
-        new Date().toLocaleDateString() +
-        ':' +
-        date.toISOString().split('T')[1].substr(0, 8)
+    // getMonth start from 0
+    let month = new Date().getMonth() + 1 + ''
 
+    if (month.length === 1) month = '0' + month
+
+    let YEMMDD =
+        new Date().getDate() + '.' + month + '.' + new Date().getFullYear()
+
+    let createDate =
+        YEMMDD + ':' + date.toISOString().split('T')[1].substr(0, 8)
+
+    // console.log(new Date().toLocaleDateString()) // regional changes from dd.mm.year to mm/dd/year
     return createDate
 }
